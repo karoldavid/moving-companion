8b46ba82c0ef37265928854ea75e8f41dfdb4e5e
lorak <k.zysk@zoho.com>
2015-06-03 09:52:21 +0200

    add streetview background picture

--- js/script.js
+++ js/script.js
@@ -11,10 +11,12 @@ function loadData() {
     $wikiElem.text("");
     $nytElem.text("");
 
-    // load streetview
-
-    // YOUR CODE GOES HERE!
-
+    var streetStr = $('#street').val();
+    var cityStr = $('#city').val();
+    var address = streetStr + ',' + cityStr;
+    $greeting.text('So you want to live at ' + address + '?');
+    var streetviewUrl = 'https://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
+    $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
     return false;
 };
 
