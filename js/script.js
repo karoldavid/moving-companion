0302f95ddadff734dd115b22efe7fb2edd4bf02f
lorak <k.zysk@zoho.com>
2016-08-26 16:00:57 +0200

    refactor: remove console.log statements

--- js/script.js
+++ js/script.js
@@ -19,10 +19,6 @@ function loadData() {
     var streetviewUrl = 'https://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '&key=AIzaSyDB2twrWRm7tw2ZtFIWakViLeMz58U9QL8';
     $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
 
-    https://maps.googleapis.com/maps/api/streetview?size=600x300&location=46.414382,10.013988&heading=151.78&pitch=-0.76&key=YOUR_API_KEY
-
-    console.log(streetviewUrl);
-
     // load nytimes
     var nytApiKey = '0ecda6d95b828817edbcfef8445c1aeb:7:72206312';
     var nytBaseUrl = 'https://api.nytimes.com/svc/search/v2/articlesearch.json';
@@ -31,7 +27,6 @@ function loadData() {
 
     $.getJSON(nytUrl, function (data) {
     	$nytHeaderElem.text('New York Times Articles About ' + cityStr);
-    	console.log("data: ", data);
     	var articles = data.response.docs;
     	$.each( articles, function( i, article) {
             $nytElem.append('<li class="article"><a href=' + article.web_url + '>' + article.headline.main + '</a><p>' + article.snippet + '</p></li>');
